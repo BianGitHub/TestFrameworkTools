9863fdb35fcbf381e0bff3d07b21ae1597096ab8
边雷 <lei.bian2@Pactera-fintech.com>
2021-03-30 17:55:52 +0800

    fix

---
+++ TaoTestSDK.framework/Headers/TaoTestModel.h
@@ -0,0 +1,18 @@
+//
+//  TaoTestModel.h
+//  TaoTestSDK
+//
+//  Created by 涛胖子 on 2021/3/30.
+//  Copyright © 2021 TaoTestSDK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TaoTestModel : NSObject
+
++ (void)taoTest;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TaoTestSDK.framework/Headers/TaoTestSDK.h
@@ -0,0 +1,20 @@
+//
+//  TaoTestSDK.h
+//  TaoTestSDK
+//
+//  Created by 涛胖子 on 2021/3/30.
+//  Copyright © 2021 TaoTestSDK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TaoTestSDK.
+FOUNDATION_EXPORT double TaoTestSDKVersionNumber;
+
+//! Project version string for TaoTestSDK.
+FOUNDATION_EXPORT const unsigned char TaoTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaoTestSDK/PublicHeader.h>
+
+
+#import <TaoTestSDK/TaoTestModel.h>
