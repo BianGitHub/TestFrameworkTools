787fbbd6619062564eb913016164bf66d5cc1998
边雷 <lei.bian2@Pactera-fintech.com>
2021-03-30 10:03:59 +0800

    commit

---
+++ Frameworks/BLTools.framework/Headers/ToolsTest.h
@@ -0,0 +1,17 @@
+//
+//  ToolsTest.h
+//  BLTools
+//
+//  Created by 边雷 on 2021/3/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ToolsTest : NSObject
+- (void)test;
+- (void)test2;
+@end
+
+NS_ASSUME_NONNULL_END
